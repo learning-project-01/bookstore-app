3e9015b1401f6f9260d4a71ce242ae9a4cc898f3
rkpatra201 <rabindrapatra825@gmail.com>
2024-01-17 10:19:59 +0530

    added flyway configuration to manage db schema

---
+++ src/main/java/com/example/bookstoreapp/configs/FlywayConfig.java
@@ -0,0 +1,46 @@
+package com.example.bookstoreapp.configs;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Configuration class to manage Flyway database migrations.
+ */
+@Configuration
+@Slf4j
+public class FlywayConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    /**
+     * Executes Flyway migration on application startup.
+     */
+    @PostConstruct
+    public void init() {
+        log.info("Executing Flyway migration");
+        executeFlyway(dataSource);
+        log.info("Flyway migration executed");
+    }
+
+    /**
+     * Executes Flyway migration using the provided DataSource.
+     *
+     * @param dataSource DataSource used for database connection.
+     */
+    private void executeFlyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+            .dataSource(dataSource)
+            .locations("classpath:/db/scripts") // Replace this with your custom path
+            .load();
+
+        // You can set other Flyway configurations here if needed
+
+        flyway.migrate();
+    }
+}
