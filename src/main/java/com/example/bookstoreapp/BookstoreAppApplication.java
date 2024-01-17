863f6548d8693ece1daca870c1581d95e95484d1
rkpatra201 <rabindrapatra825@gmail.com>
2024-01-17 08:25:08 +0530

    initial commit

---
+++ src/main/java/com/example/bookstoreapp/BookstoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAppApplication.class, args);
+	}
+
+}
