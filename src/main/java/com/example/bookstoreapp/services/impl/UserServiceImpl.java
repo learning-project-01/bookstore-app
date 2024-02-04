3f0400657217ca41b3241f81b2879a5cbb2c7ccd
rabindrapatra <rabindrapatra@corp.com>
2024-02-04 18:31:37 +0530

    added login api

--- src/main/java/com/example/bookstoreapp/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/bookstoreapp/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.bookstoreapp.services.impl;
 
 import com.example.bookstoreapp.entities.UserEntity;
+import com.example.bookstoreapp.exceptions.AppRuntimeException;
 import com.example.bookstoreapp.models.User;
 import com.example.bookstoreapp.repositories.UserEntityRepository;
 import com.example.bookstoreapp.services.UserService;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,8 +24,19 @@ public class UserServiceImpl implements UserService {
   public User createUser(User user) {
     user.setId(System.nanoTime());
     String rawPassword = user.getPassword();
-    user.setPassword(passwordEncoder.encode(rawPassword));
+    user.setPassword(passwordEncoder.encode(rawPassword)); // hashing
     UserEntity userEntity = userEntityRepository.save(user.toEntity());
     return new User().fromEntity(userEntity);
+  }
+
+  @Override
+  public String authenticate(User user) {
+    UserEntity entity = userEntityRepository.findByEmail(user.getEmail());
+    String encodedPassword = entity.getPassword();
+    boolean passwordMatched = passwordEncoder.matches(user.getPassword(), encodedPassword);
+    if(passwordMatched){
+      return UUID.randomUUID().toString();
+    }
+    throw new AppRuntimeException("login failed");
   }
 }
