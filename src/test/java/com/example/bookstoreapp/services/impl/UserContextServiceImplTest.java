9a1d11bbe329890766d4fd359f4e11e6d45b938b
rabindrapatra <rabindrapatra@corp.com>
2024-02-14 20:53:01 +0530

    fix failed test

--- src/test/java/com/example/bookstoreapp/services/impl/UserContextServiceImplTest.java
+++ src/test/java/com/example/bookstoreapp/services/impl/UserContextServiceImplTest.java
@@ -1,19 +1,25 @@
 package com.example.bookstoreapp.services.impl;
 
+import com.example.bookstoreapp.models.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(MockitoExtension.class)
 class UserContextServiceImplTest {
-    @InjectMocks
-    UserContextServiceImpl userSessionServiceImps;
+
+    private UserContextServiceImpl userContextService;
+
+    @BeforeEach
+    public void beforeEach(){
+        userContextService = new UserContextServiceImpl();
+        User user = new User();
+        user.setId(434175530633000L);
+        userContextService.setUser(user);
+    }
 
     @Test
     void getUserId() {
-        assertEquals(434175530633000L, userSessionServiceImps.getUserId());
+        assertEquals(434175530633000L, userContextService.getUserId());
     }
 }
