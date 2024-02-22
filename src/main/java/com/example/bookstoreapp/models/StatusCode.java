e2855f574e4224d8dcfda472262900d1c4f993bd
rabindrapatra <rabindrapatra@corp.com>
2024-02-22 12:04:10 +0530

    added status code value mapping

--- src/main/java/com/example/bookstoreapp/models/StatusCode.java
+++ src/main/java/com/example/bookstoreapp/models/StatusCode.java
@@ -1,18 +1,40 @@
 package com.example.bookstoreapp.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum StatusCode {
 
   PLACED(0),
   APPROVED(1),
   IN_TRANSIT(2),
   DELIVERY_COMPLETE(3),
-  REFUND_IN_TRANSIT(4),
-  REFUND_APPROVED(5),
-  REFUNDED(6);
+  RETURN_IN_TRANSIT(4),
+  RETURN_APPROVED(5),
+  REFUNDED(6),
+  RETURN_REQUESTED(7),
+  RETURN_PICKED_UP(8),
+  RETURN_RECIEVED(9);
 
-  int weight;
+  int value;
 
-  StatusCode(int weight) {
-    this.weight = weight;
+  StatusCode(int value) {
+    this.value = value;
   }
+
+  public int getValue() {
+    return value;
+  }
+
+  private static final Map<Integer, StatusCode> valueVsStatusCode = new HashMap<>();
+  
+  public StatusCode toStatusCode(int value){
+    if(valueVsStatusCode.isEmpty()){
+      for(StatusCode statusCode: StatusCode.values()){
+        valueVsStatusCode.put(statusCode.value, statusCode);
+      }
+    }
+    return valueVsStatusCode.get(value);
+  }
+
 }
