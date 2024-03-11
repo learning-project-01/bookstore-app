bdf5e8c652e4a0f0185558d4d5667d1b2c0e5f6e
Ansu-s <csahoo222@gmail.com>
2024-03-11 12:05:23 +0530

    Item limit per cart added

--- src/main/java/com/example/bookstoreapp/entities/CatalogItemEntity.java
+++ src/main/java/com/example/bookstoreapp/entities/CatalogItemEntity.java
@@ -12,19 +12,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CatalogItemEntity {
 
-  @Id
-  private Long id;
+    @Id
+    private Long id;
 
-  private String name;
+    private String name;
 
-  private Float price;
+    private Float price;
 
-  private Integer stockQuantity;
+    private Integer stockQuantity;
 
-  public CatalogItemEntity(Long id, String name, Float price, Integer stockQuantity) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.stockQuantity = stockQuantity;
-  }
+    private Integer itemLimit;
+
+    public CatalogItemEntity(Long id, String name, Float price, Integer stockQuantity, Integer itemLimit) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.itemLimit = itemLimit;
+    }
 }
--- src/main/java/com/example/bookstoreapp/models/CartItem.java
+++ src/main/java/com/example/bookstoreapp/models/CartItem.java
@@ -7,26 +7,27 @@ import lombok.Data;
 @Data
 public class CartItem extends CatalogItem {
 
-  private Long cartId;
-  private int quantity;
-  private Float total;
-  private Float unitPrice;
+    private Long cartId;
+    private int quantity;
+    private Float total;
+    private Float unitPrice;
 
-  private CartItemState cartItemState;
+    private CartItemState cartItemState;
 
-  private boolean outOfStock;
-  private String alertMessage; // some message like: item out of stock or 2 quantities available only
+    private boolean outOfStock;
+    private String alertMessage; // some message like: item out of stock or 2 quantities available only
 
 
-  public CartItem fromEntity(CartItemEntity cartItemEntity, CatalogItem catalogItem) {
-    super.setId(catalogItem.getId());
-    super.setName(catalogItem.getName());
-    super.setPrice(catalogItem.getPrice());
-    this.setCartId(cartItemEntity.getCartId());
-    this.setCartItemState(CartItemState.mapWeighToState(cartItemEntity.getState()));
-    this.setQuantity(cartItemEntity.getQuantity());
-    this.setUnitPrice(catalogItem.getPrice());
-    this.setTotal(this.getUnitPrice() * this.getQuantity());
-    return this;
-  }
+    public CartItem fromEntity(CartItemEntity cartItemEntity, CatalogItem catalogItem) {
+        super.setId(catalogItem.getId());
+        super.setName(catalogItem.getName());
+        super.setPrice(catalogItem.getPrice());
+        super.setItemLimit(catalogItem.getItemLimit());
+        this.setCartId(cartItemEntity.getCartId());
+        this.setCartItemState(CartItemState.mapWeighToState(cartItemEntity.getState()));
+        this.setQuantity(cartItemEntity.getQuantity());
+        this.setUnitPrice(catalogItem.getPrice());
+        this.setTotal(this.getUnitPrice() * this.getQuantity());
+        return this;
+    }
 }
