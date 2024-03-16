0ce04c645bd013f1322dc838bacc51984edf823f
rabindrapatra <rabindrapatra@corp.com>
2024-03-16 23:41:22 +0530

    fix wildcard import statements

--- src/main/java/com/example/bookstoreapp/controllers/CartController.java
+++ src/main/java/com/example/bookstoreapp/controllers/CartController.java
@@ -5,7 +5,13 @@ import com.example.bookstoreapp.models.CartItem;
 import com.example.bookstoreapp.models.CartItemStateUpdate;
 import com.example.bookstoreapp.services.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cart")
@@ -28,9 +34,9 @@ public class CartController {
   public Cart checkout(){
     return cartItemService.doCheckout(0L);
   }
+
   @PutMapping("/{catalogItemId}/state")
-  public CartItemStateUpdate updateState(@PathVariable Long catalogItemId, @RequestBody CartItem cartItem)
-  {
-    return cartItemService.updateState(catalogItemId,cartItem);
+  public CartItemStateUpdate updateState(@PathVariable Long catalogItemId, @RequestBody CartItemStateUpdate state) {
+    return cartItemService.updateState(catalogItemId, state);
   }
 }
