bb1822bb90fddf81254b7819cee8276f78cd0f2f
rabindrapatra <rabindrapatra@corp.com>
2024-03-16 22:55:55 +0530

    cleanup activity for unused classes

--- src/main/java/com/example/bookstoreapp/services/CartItemStateService.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstoreapp.services;
-
-import com.example.bookstoreapp.models.CartItemStateUpdate;
-
-public interface CartItemStateService {
-    CartItemStateUpdate updateStste(Long catalogItemId, CartItemStateUpdate cartItemStateUpdate);
-}
