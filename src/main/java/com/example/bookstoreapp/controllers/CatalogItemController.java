a2f8b06302536677774ccc4625df931a25e1e042
Ansu-s <130679461+Ansu-s@users.noreply.github.com>
2025-04-06 23:02:30 +0530

    distributed tracing using opentelemetry and zipkin

--- src/main/java/com/example/bookstoreapp/controllers/CatalogItemController.java
+++ src/main/java/com/example/bookstoreapp/controllers/CatalogItemController.java
@@ -2,6 +2,10 @@ package com.example.bookstoreapp.controllers;
 
 import com.example.bookstoreapp.models.CatalogItem;
 import com.example.bookstoreapp.services.CatalogItemService;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,26 +23,54 @@ import java.util.List;
 @RequestMapping("/catalogItems") // URL is plural here i.e. catalogItems
 public class CatalogItemController {
 
-  @Autowired
-  private CatalogItemService catalogItemService;
+    @Autowired
+    private CatalogItemService catalogItemService;
 
+    @Autowired
+    private Tracer tracer;
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  // as the item will be created after the execution of below method,
-  // so we will return HTTPStatus as CREATED i.e. 201
-  public CatalogItem createNewItem(@RequestBody CatalogItem catalogItem) {
-    return catalogItemService.create(catalogItem);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    // as the item will be created after the execution of below method,
+    // so we will return HTTPStatus as CREATED i.e. 201
+    public CatalogItem createNewItem(@RequestBody CatalogItem catalogItem) {
+        Span span = tracer.spanBuilder("CreateCatalogItem")
+                .setSpanKind(SpanKind.SERVER)
+                .startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            span.setAttribute("action", "create");
+            span.setAttribute("catalogItem.name", catalogItem.getName());
+            return catalogItemService.create(catalogItem);
+        } finally {
+            span.end();
+        }
+    }
 
-  @PutMapping("/{id}")
-  @ResponseStatus(HttpStatus.OK)
-  public CatalogItem updateItem(@PathVariable Long id, @RequestBody CatalogItem catalogItem) {
-    return catalogItemService.update(id, catalogItem);
-  }
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CatalogItem updateItem(@PathVariable Long id, @RequestBody CatalogItem catalogItem) {
+        Span span = tracer.spanBuilder("UpdateCatalogItem")
+                .setSpanKind(SpanKind.SERVER)
+                .startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            span.setAttribute("action", "update");
+            span.setAttribute("catalogItem.id", id);
+            return catalogItemService.update(id, catalogItem);
+        } finally {
+            span.end();
+        }
+    }
 
-  @GetMapping
-  public List<CatalogItem> listItems() {
-    return catalogItemService.list();
-  }
+    @GetMapping
+    public List<CatalogItem> listItems() {
+        Span span = tracer.spanBuilder("ListCatalogItems")
+                .setSpanKind(SpanKind.SERVER)
+                .startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            span.setAttribute("action", "list");
+            return catalogItemService.list();
+        } finally {
+            span.end();
+        }
+    }
 }
--- src/main/java/com/example/bookstoreapp/controllers/HealthController.java
+++ src/main/java/com/example/bookstoreapp/controllers/HealthController.java
@@ -1,5 +1,11 @@
 package com.example.bookstoreapp.controllers;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +20,31 @@ import java.util.Map;
 @RequestMapping
 public class HealthController {
 
+  @Autowired
+  private Tracer tracer;
+
+
   @Value("${application.name}")
   private String appName;
 
   @GetMapping("/health")
   public Map<String, String> getHealth() {
-    Map<String, String> map = new HashMap<>();
-    map.put("appName", appName);
-    map.put("systemTime", Instant.now().atZone(ZoneId.of("UTC")).toLocalDateTime().toString());
-    return map;
+
+    Span span = tracer.spanBuilder("HealthCheck")
+            .setSpanKind(SpanKind.SERVER)
+            .startSpan();
+
+    try (Scope scope = span.makeCurrent()) {
+      span.setAttribute("health.endpoint", "/api/health");
+      span.setAttribute("application.name", appName);
+
+      Map<String, String> map = new HashMap<>();
+      map.put("appName", appName);
+      map.put("systemTime", Instant.now().atZone(ZoneId.of("UTC")).toLocalDateTime().toString());
+
+      return map;
+    } finally {
+      span.end();
+    }
   }
 }
